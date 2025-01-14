db61dc385f3284f5f1f428e18a58cfb6ebe64b75
stephan.fuchs <stephan.fuchs@robotron.de>
2025-01-14 22:29:07 +0100

    Overlap

--- isochrone_karte.py
+++ isochrone_karte.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup as bs
 import openrouteservice as ors  # Für die Berechnung der Distanzen
 import secret_api   # eigene Datei mit API. Dateiinhalt:   api="5b3c....."
 import folium
-
+from shapely.geometry import Polygon, LineString, Point
+import geopandas
 """ 
 Ideen:
 Aufteilung nach Fahrrad, Auto
@@ -26,14 +27,15 @@ client = ors.Client(key=secret_api.api)
 m = folium.Map(location=[51.05885076550623, 13.766713420144118], tiles='OpenStreetMap', zoom_start=13)
 
 farben = ["00ff00","lightgreen","red","orange","pink","darkgreen"]
-# Also create a Popup with Population count within isochrones
+geo_dict={"name":[],"geometry":[]}
+geo_arrays = []
 i=0
 for coordi in Ziel_Adressen:
     print(i,coordi,farben[i])
     iso = client.isochrones(
     locations=[Ziel_Adressen[coordi][::-1]],
     profile='driving-car',  #'foot-walking',  'driving-car' 'cycling-regular'
-    range=[600,900,1200,1500,1800],  # Seconds
+    range=[1800],#,900,1200,1500,1800],  # Seconds
     validate=False
     )
 
@@ -41,8 +43,16 @@ for coordi in Ziel_Adressen:
 
     for isochrone in iso['features'][::-1]:
         #print(isochrone)
+        locations=[list(reversed(coord)) for coord in isochrone['geometry']['coordinates'][0]]
+        #print("locations",locations)
+        geo_dict["name"].append(coordi)
+        lat=[ i[1] for i in locations]
+        long=[ i[0] for i in locations]
+        polygon_geom = Polygon(zip(lat,long))
+        geo_dict["geometry"].append(polygon_geom)
+
         
-        folium.Polygon(locations=[list(reversed(coord)) for coord in isochrone['geometry']['coordinates'][0]],
+        folium.Polygon(locations=locations,
                     fillColor="00ff00",#farben[i],
                     #color="black",
                     popup=folium.Popup(coordi+" "+str(round(isochrone["properties"]["value"]/60)) + " min"),
@@ -58,8 +68,20 @@ for coordi in Ziel_Adressen:
         
     i=i+1
 
+
+polygon_df = geopandas.GeoDataFrame(data=geo_dict, crs='epsg:4326',index=geo_dict["name"])#, geometry=[polygon_geom])       
+
+print("DF: ",polygon_df)
+
+ausgabe=polygon_df["geometry"].loc["Robotron"]
+
+for namen in polygon_df["name"]:
+    ausgabe=ausgabe.intersection(polygon_df["geometry"].loc[namen])
+
+folium.GeoJson(ausgabe,fillColor="red",color="red",name="Overlap").add_to(m)
+ 
+#print("Ausgabe",ausgabe)
+
 folium.LayerControl().add_to(m)
-
-
 file_name = 'my_folium_map'
 m.save(file_name + '.html')
