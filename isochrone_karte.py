746c8b8df95d2eb7b315ea95fc509aebaf9df71a
stephan.fuchs <stephan.fuchs@robotron.de>
2025-01-14 10:26:32 +0100

    Mit Layer in Karte

--- isochrone_karte.py
+++ isochrone_karte.py
@@ -21,6 +21,7 @@ client = ors.Client(key=secret_api.api)
 
 m = folium.Map(location=[51.05885076550623, 13.766713420144118], tiles='OpenStreetMap', zoom_start=13)
 
+
 # Some coordinate in Berlin
 coordinate = [[13.384116, 52.533558]]
 coordinate = list(Ziel_Adressen.values())
@@ -34,25 +35,35 @@ for coordi in Ziel_Adressen:
     iso = client.isochrones(
     locations=[Ziel_Adressen[coordi][::-1]],
     profile='driving-car',  #'foot-walking',  'driving-car' 'cycling-regular'
-    range=[600],#, 900,1200],#,1500,1800],  # Seconds
+    range=[600,900,1200,1500,1800],  # Seconds
     validate=False,
     #attributes=['total_pop']
     )
     #print("iso",iso)
-    for isochrone in iso['features']:
-        print(isochrone)
-        folium.Polygon(locations=[list(reversed(coord)) for coord in isochrone['geometry']['coordinates'][0]],
-                    fillColor=farben[i],
-                        color="black",
-                    popup=folium.Popup(coordi+" "+str(isochrone["properties"]["value"])),
-                    opacity=0.9).add_to(m)
+
+    fg = folium.FeatureGroup(name=coordi, control=True, overlay=True).add_to(m)
+
+    for isochrone in iso['features'][::-1]:
+        #print(isochrone)
         
-        folium.map.Marker(list(reversed(Ziel_Adressen[coordi])),  # reverse coords due to weird folium lat/lon syntax
+        folium.Polygon(locations=[list(reversed(coord)) for coord in isochrone['geometry']['coordinates'][0]],
+                    fillColor="00ff00",#farben[i],
+                    #color="black",
+                    popup=folium.Popup(coordi+" "+str(round(isochrone["properties"]["value"]/60)) + " min"),
+                    opacity=0.5).add_to(fg)
+        
+        folium.map.Marker((Ziel_Adressen[coordi]),  # reverse coords due to weird folium lat/lon syntax
                     icon=folium.Icon(color='lightgray',
                                        icon_color='#cc0000',
                                        icon='home',
                                        prefix='fa',
-                                       ), popup=coordi,).add_to(m)
+                                       ), popup=coordi,).add_to(fg)
+        
+        
     i=i+1
+
+folium.LayerControl().add_to(m)
+
+
 file_name = 'my_folium_map'
 m.save(file_name + '.html')
